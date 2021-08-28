60fe02a8eac0e94460d95e07f10d62f462ef30e4
Matheus Avellar <MatheusAvellar@users.noreply.github.com>
2021-08-27 23:58:44 -0300

    Adiciona gráficos, [abc]XXX.br, email

--- brute-force.sh
+++ brute-force.sh
@@ -6,13 +6,17 @@ echo "" > res.txt
 echo "Querying SLDs..."
 i=0
 # For every SLD in the list ({a..z}{a..z} checks aa.br, ab.br, ac.br, ...)
-for sld in {a..z}{0..9}{0..9}
+for sld in d{a..z}{a..z}{a..z}
 do
   i=$((i+1))
-  echo -n "$sld.br ($i/2600)  "
+  echo -n "$sld.br ($i/17576)  "
   # Kill query if it doesn't finish after 30s
   timeout 30s host $sld.br >> res.txt
   echo -n "[root ✓] "
+  timeout 30s host -t NS $sld.br >> res.txt
+  echo -n "[NS ✓] "
+  timeout 30s host -t SOA $sld.br >> res.txt
+  echo -n "[SOA ✓] "
   # Test for www. as well (eg. unicamp.br)
   timeout 30s host www.$sld.br >> res.txt
   echo "[www ✓]"
@@ -39,7 +43,13 @@ echo "" >> out.txt
 echo "Tangential AAAA records -------"                      | tee -a out.txt
 grep -iE "^([a-z0-9\-]+\.){3,}br has IPv6" res.txt          | sort -uf | tee -a out.txt
 echo "" >> out.txt
-echo "Tangential MX records -------------"                  | tee -a out.txt
+echo "Tangential MX records ---------"                      | tee -a out.txt
 grep -iE "^([a-z0-9\-]+\.){3,}br mail is handled" res.txt   | sort -uf | tee -a out.txt
+echo "" >> out.txt
+echo "NS records --------------------"                      | tee -a out.txt
+grep -iE "^[a-z0-9\-]+\.br name server" res.txt             | sort -uf | tee -a out.txt
+echo "" >> out.txt
+echo "SOA records -------------------"                      | tee -a out.txt
+grep -iE "^[a-z0-9\-]+\.br has SOA record" res.txt          | sort -uf | tee -a out.txt
 
 rm res.txt
